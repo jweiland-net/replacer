87f62400779ad93fb93b09fa7c3f6bf2c4be8bfc
Alexander Bohndorf <bohndorf@sitegeist.de>
2018-09-27 08:47:51 +0200

    Upgrade to work with TYPO3 9.x (#2)
    * Use contentPostProc-all instead of contentPostProc-output
    
    If content comes from cache the usage of stdWrap fails because cObj is not set.
    To fix the issue i register the contentPostProc-all hook to make replacements before writing to cache.
    
    * set version to 1.2.0
    
    * Update for TYPO3 9 and added pull request from  ger4003 to use contentPostProc-all instead of contentPostProc-output
    
    * [TASK] Make compatible with TYPO3 7.6 - 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '7.6.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
