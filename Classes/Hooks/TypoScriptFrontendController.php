c6109a0ad46c05ce62a9043cd397703a3d910b53
Stefan Froemken <froemken@gmail.com>
2021-03-04 14:47:00 +0100

    Add php-cs-fixer config file

--- Classes/Hooks/TypoScriptFrontendController.php
+++ Classes/Hooks/TypoScriptFrontendController.php
@@ -1,19 +1,15 @@
 <?php
 
-namespace JWeiland\Replacer\Hooks;
+declare(strict_types=1);
 
 /*
-* This file is part of the TYPO3 CMS project.
-*
-* It is free software; you can redistribute it and/or modify it under
-* the terms of the GNU General Public License, either version 2
-* of the License, or any later version.
-*
-* For the full copyright and license information, please read the
-* LICENSE.txt file that was distributed with this source code.
-*
-* The TYPO3 project - inspiring people to share!
-*/
+ * This file is part of the package jweiland/replacer.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Replacer\Hooks;
 
 use TYPO3\CMS\Core\Log\LogLevel;
 use TYPO3\CMS\Core\Log\LogManager;
