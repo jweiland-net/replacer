729bf70b901cdfae95a955d58326e4ffb21ca747
Marcus Förster <m.foerster@xerc.de>
2022-06-02 12:01:27 +0200

    [ADD] scriptmerger conflict @ composer

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,4 @@
-<?php
-defined('TYPO3_MODE') or die();
+<?php defined('TYPO3_MODE') || die();
 
 // Register hook for cached content
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['z9_replacer'] =
