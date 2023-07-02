f3ae637ba46a74301397625fe5c7e412b5e2747d
Marcus Förster <m.foerster@xerc.de>
2023-07-02 21:17:23 +0200

    [ACT] typo3 v12 #20

--- Classes/Middleware/ReplaceContent.php
+++ Classes/Middleware/ReplaceContent.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace JWeiland\Replacer\Middleware;
 
-use JWeiland\Replacer\Helper\ReplacerHelper;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -19,26 +18,28 @@ use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Http\NullResponse;
 use TYPO3\CMS\Core\Http\Stream;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 /**
  * Middleware to replace content using TSFE.
- * Will be used for pages with USER_INT plugins only!
- * Otherwise Hooks\TypoScriptFrontendController will replace the content.
  */
 class ReplaceContent implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $response = $handler->handle($request);
-        if (!$GLOBALS['TSFE']->isINTincScript() || $response instanceof NullResponse) {
-            return $response;
+        if (!($response instanceof NullResponse)
+        && $GLOBALS['TSFE'] instanceof TypoScriptFrontendController
+        && 'text/html' == substr($response->getHeaderLine('Content-Type'), 0, 9)
+        && !empty($response->getBody())
+        ) {
+            $content = GeneralUtility::makeInstance(\JWeiland\Replacer\Helper\ReplacerHelper::class)
+                ->replace((string) $response->getBody(), $GLOBALS['TSFE']);
+
+            $responseBody = new Stream('php://temp', 'rw');
+            $responseBody->write($content);
+            $response = $response->withBody($responseBody);
         }
-        $content = GeneralUtility::makeInstance(ReplacerHelper::class)
-            ->replace((string)$response->getBody(), $GLOBALS['TSFE']);
 
-        $body = new Stream('php://temp', 'rw');
-        $body->write($content);
-
-        return $response->withBody($body);
+        return $response;
     }
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '9.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
