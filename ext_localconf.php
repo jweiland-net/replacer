fbf0480fe2ecf847f309e8356ca1b14d0338470e
Marcus Förster <m.foerster@xerc.de>
2023-07-02 21:17:23 +0200

    [ACT] typo3 v12 #20

--- ext_localconf.php
+++
@@ -1,5 +0,0 @@
-<?php defined('TYPO3_MODE') || die();
-
-// Register hook for cached content
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['z9_replacer'] =
-    \JWeiland\Replacer\Hooks\TypoScriptFrontendController::class . '->contentPostProcAll';
