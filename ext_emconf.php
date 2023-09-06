1e29f6a26f9b8c5feb911517d48b7bf00e55f037
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2023-09-06 11:58:29 +0200

    - Extension Version and Compatibility Changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,21 +4,19 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Content Replacer',
     'description' => 'Replaces string patterns from the page. You can use it to replace URLs for Content Delivery Network (CDN).',
     'category' => 'fe',
-    'author' => 'Pascal Rinker',
+    'author' => 'Hoja Mustaffa Abdul Latheef',
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.1.1',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '11.5.30-12.4.99',
         ],
         'conflicts' => [],
-        'suggests' => [
-            'scriptmerger' => '7.0.0-0.0.0',
-        ],
+        'suggests' => [],
     ],
 ];
