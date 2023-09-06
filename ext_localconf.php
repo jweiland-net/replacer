b87b3e850de9175125441466073fddb180d01f58
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2023-09-06 11:47:12 +0200

    [Task] Compatibilty Fix for ext_localconf to adapt TYPO3 Version 12 and 11
    - changed mode checking conditions
    - version compare for support hook implementation for TYPO3 11LTS

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,17 @@
-<?php defined('TYPO3_MODE') || die();
+<?php
+if (!defined('TYPO3_MODE') && !defined('TYPO3')) {
+    die('Access denied.');
+}
 
-// Register hook for cached content
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['z9_replacer'] =
-    \JWeiland\Replacer\Hooks\TypoScriptFrontendController::class . '->contentPostProcAll';
+$typo3VersionUtility = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \TYPO3\CMS\Core\Information\Typo3Version::class
+);
+if (version_compare($typo3VersionUtility->getBranch(), '12.4', '>=')) {
+    // Register hook for cached content
+    // this has no effect in TYPO3 12 because the hook does'nt exists inside the core and we implemented a event listener
+    // based on PSR-14 Events 'AfterCacheableContentIsGeneratedEvent'
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['z9_replacer'] =
+        \JWeiland\Replacer\Hooks\TypoScriptFrontendController::class . '->contentPostProcAll';
+}
+// For Version above 12.4 instead of Hook implementation PSR 14 Event configured under 'Configurations/Services.yaml
+// event we used is AfterCacheableContentIsGeneratedEvent
