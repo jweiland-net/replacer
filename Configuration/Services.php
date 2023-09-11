61309f259d79da5e51911ce35c87b772364cc8a2
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2023-09-11 13:53:02 +0200

    [BUGFIX] Removed Event Listener loading in 11 because it doesn't exists

---
+++ Configuration/Services.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/replacer.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+use JWeiland\Replacer\EventListener\CacheableContentGeneratedEventListener;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+return static function (ContainerConfigurator $container, ContainerBuilder $containerBuilder) {
+    $typo3Version = GeneralUtility::makeInstance(Typo3Version::class);
+    if (version_compare($typo3Version->getBranch(), '12.0', '<')) {
+        $containerBuilder->removeDefinition(CacheableContentGeneratedEventListener::class);
+    }
+};
