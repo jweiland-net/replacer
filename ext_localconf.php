d1e59ff21ece6bb09543d7d2fe3ffeea24d37a2c
Stefan Froemken <sfroemken@jweiland.net>
2023-09-22 15:35:44 +0200

    Repair and skip some func tests for TYPO3 11

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,16 +7,13 @@ use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 call_user_func(static function () {
-    $typo3VersionUtility = GeneralUtility::makeInstance(
-        Typo3Version::class
-    );
+    $typo3VersionUtility = GeneralUtility::makeInstance(Typo3Version::class);
 
-    if (version_compare($typo3VersionUtility->getBranch(), '12.4', '>=')) {
-        // Register hook for cached content
-        // this has no effect in TYPO3 12 because the hook doesn't exist inside the core and we implemented
-        // an event listener based on PSR-14 Events 'AfterCacheableContentIsGeneratedEvent'
-        // For Version above 12.4 instead of Hook implementation PSR 14 Event configured under
-        // 'Configurations/Services.yaml event we used is AfterCacheableContentIsGeneratedEvent
+    if (version_compare($typo3VersionUtility->getBranch(), '12.0', '<')) {
+        // Register hook for cached content.
+        // This has no effect since TYPO3 12, because the hook doesn't exist inside the core anymore, and we
+        // implemented an event listener based on PSR-14 Event 'AfterCacheableContentIsGeneratedEvent' which was
+        // available since TYPO3 12.0.
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['z9_replacer'] =
             TypoScriptFrontendControllerHook::class . '->contentPostProcAll';
     }
