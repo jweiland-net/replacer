4dccb2269d2401f0cfb50eebde8569ca5762eb90
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-10-21 10:45:27 +0200

    [TASK] Update version changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the package jweiland/replacer.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Content Replacer',
     'description' => 'Replaces string patterns from the page. You can use it to replace URLs for Content Delivery Network (CDN).',
@@ -8,10 +15,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '3.0.3',
+    'version' => '3.0.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.30-12.4.99',
+            'typo3' => '11.5.37-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the package jweiland/replacer.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 defined('TYPO3') or die();
 
 use JWeiland\Replacer\Hook\TypoScriptFrontendControllerHook;
