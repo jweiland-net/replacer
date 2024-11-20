2bdf6b753f7e97934c72a5c16e5f08ccbe4f3f9f
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-11-20 13:08:25 +0100

    [TASK] Fixed phpstan issues

--- Classes/Helper/TypoScriptHelper.php
+++ Classes/Helper/TypoScriptHelper.php
@@ -11,25 +11,14 @@ declare(strict_types=1);
 
 namespace JWeiland\Replacer\Helper;
 
-use JWeiland\Replacer\Traits\GetTypoScriptFrontendControllerTrait;
 use Psr\Http\Message\ServerRequestInterface;
-use TYPO3\CMS\Core\Http\ServerRequest;
 
 class TypoScriptHelper
 {
-    use GetTypoScriptFrontendControllerTrait;
-
-    protected ServerRequestInterface $request;
-
-    public function __construct(ServerRequest $request)
-    {
-        $this->request = $request;
-    }
-
     /**
-     * @param string|int $key
+     * @param array<int, mixed> $typoScriptConfiguration
      */
-    public function hasStdWrapProperties(array $typoScriptConfiguration, $key): bool
+    public function hasStdWrapProperties(array $typoScriptConfiguration, int|string $key): bool
     {
         $possibleStdWrapConfiguration = $typoScriptConfiguration[rtrim((string)$key, '.') . '.'] ?? [];
         if (is_array($possibleStdWrapConfiguration)) {
@@ -43,9 +32,10 @@ class TypoScriptHelper
     }
 
     /**
-     * @param string|int $key
+     * @param array<int, mixed> $typoScriptConfiguration
+     * @return array<int, mixed>
      */
-    public function getStdWrapProperties(array $typoScriptConfiguration, $key): array
+    public function getStdWrapProperties(array $typoScriptConfiguration, int|string $key): array
     {
         if ($this->hasStdWrapProperties($typoScriptConfiguration, $key)) {
             $stdWrapConfiguration = $typoScriptConfiguration[rtrim((string)$key, '.') . '.'] ?? [];
@@ -64,10 +54,11 @@ class TypoScriptHelper
      * It can be either "10", but also "10." where "10" has priority.
      * In case of just "10" we will add stdWrap properties automatically
      *
-     * @param string|int $key
-     * @return string|array
+     * @param array<int, string> $typoScriptConfiguration
+     * @param int|string $key
+     * @return array<int, mixed>|string
      */
-    public function findValueOrConfiguration(array $typoScriptConfiguration, $key)
+    public function findValueOrConfiguration(array $typoScriptConfiguration, int|string $key): string|array
     {
         return $typoScriptConfiguration[rtrim((string)$key, '.')]
             ?? $typoScriptConfiguration[rtrim((string)$key, '.') . '.']
@@ -75,9 +66,9 @@ class TypoScriptHelper
     }
 
     /**
-     * @param string|int $key
+     * @param array<int, string> $typoScriptConfiguration
      */
-    public function isRegExpEnabled(array $typoScriptConfiguration, $key): bool
+    public function isRegExpEnabled(array $typoScriptConfiguration, int|string $key): bool
     {
         $subTypoScriptConfiguration = $typoScriptConfiguration[rtrim((string)$key, '.') . '.'] ?? [];
 
@@ -89,9 +80,9 @@ class TypoScriptHelper
     /**
      * It will return true, if there is a configuration "10" given for $key "10."
      *
-     * @param string|int $key
+     * @param array<int, string> $typoScriptConfiguration
      */
-    public function hasBaseEntry(array $typoScriptConfiguration, $key): bool
+    public function hasBaseEntry(array $typoScriptConfiguration, int|string $key): bool
     {
         return isset($typoScriptConfiguration[rtrim((string)$key, '.')]);
     }
@@ -99,9 +90,9 @@ class TypoScriptHelper
     /**
      * It will return true, if there is a replacement configuration found return
      *
-     * @param string|int $key
+     * @param array<int, string> $typoScriptConfiguration
      */
-    public function hasReplaceEntry(array $typoScriptConfiguration, $key): bool
+    public function hasReplaceEntry(array $typoScriptConfiguration, int|string $key): bool
     {
         // depends on search keys here it will be either 10 or 10. so we need to handle here
         $keyCombinedWithTypoScriptPointer = rtrim((string)$key, '.') . '.';
@@ -109,18 +100,17 @@ class TypoScriptHelper
             || isset($typoScriptConfiguration[$keyCombinedWithTypoScriptPointer]);
     }
 
-    public function applyStdWrapProperties(string $content, array $stdWrapConfiguration): string
+    /**
+     * @param array<int, string> $stdWrapConfiguration
+     */
+    public function applyStdWrapProperties(string $content, array $stdWrapConfiguration, ServerRequestInterface $request): string
     {
-        $contentObjectRenderer = $this->getContentObjectRenderer();
+        $typoscriptFrontendController = $request->getAttribute('frontend.controller');
+        $contentObjectRenderer = $typoscriptFrontendController->cObj;
         if ($contentObjectRenderer === null) {
             return $content;
         }
 
         return $contentObjectRenderer->stdWrap($content, $stdWrapConfiguration);
-    }
-
-    protected function getRequest(): ServerRequestInterface
-    {
-        return $this->request;
     }
 }
