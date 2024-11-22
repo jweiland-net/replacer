49e379b25b7b598c3f27a081fcdf57af54561926
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-11-22 12:53:33 +0100

    [TASK] Fixed CGL issues

--- Classes/Helper/TypoScriptHelper.php
+++ Classes/Helper/TypoScriptHelper.php
@@ -12,8 +12,6 @@ declare(strict_types=1);
 namespace JWeiland\Replacer\Helper;
 
 use Psr\Http\Message\ServerRequestInterface;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 class TypoScriptHelper
 {
