e07fac207d51f95ea8e75c1fbf4cb9d526bd3234
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-08-01 11:24:52 +0200

    [TASK] Fixed Enumeration Class deprecations

--- Classes/Enumeration/ConfigurationTypeEnumeration.php
+++ Classes/Enumeration/ConfigurationTypeEnumeration.php
@@ -11,21 +11,15 @@ declare(strict_types=1);
 
 namespace JWeiland\Replacer\Enumeration;
 
-use TYPO3\CMS\Core\Type\Enumeration;
-
 /**
  * Contains the TypoScript configuration type like "search" or "replace"
  */
-final class ConfigurationTypeEnumeration extends Enumeration
+enum ConfigurationTypeEnumeration: string
 {
-    public const __default = self::TYPE_SEARCH;
-    public const TYPE_SEARCH = 'search';
-    public const TYPE_REPLACE = 'replace';
+    case SEARCH = 'search';
+    case REPLACE = 'replace';
 
-    /**
-     * We need to add a trailing dot to find the expected configuration in TypoScript
-     */
-    public function __toString()
+    public function getValueAsFormatted(): string
     {
         return $this->value . '.';
     }
