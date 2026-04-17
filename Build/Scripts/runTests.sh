0a5dfc49475b48c508cf5b33922d1693c5b5ab3e
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-04-17 13:35:48 +0200

    [TASK] Update .gitignore, runTests.sh defaults, and PHP-CS-Fixer rules

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -60,8 +60,8 @@ handleDbmsOptions() {
                 echo "Use \".Build/Scripts/runTests.sh -h\" to display help and valid options" >&2
                 exit 1
             fi
-            [ -z "${DBMS_VERSION}" ] && DBMS_VERSION="10.4"
-            if ! [[ ${DBMS_VERSION} =~ ^(10.4|10.5|10.6|10.7|10.8|10.9|10.10|10.11|11.0|11.1)$ ]]; then
+            [ -z "${DBMS_VERSION}" ] && DBMS_VERSION="10.11"
+            if ! [[ ${DBMS_VERSION} =~ ^(10.11|11.0|11.1|11.2|11.3|11.4|11.5|11.6)$ ]]; then
                 echo "Invalid combination -d ${DBMS} -i ${DBMS_VERSION}" >&2
                 echo >&2
                 echo "Use \".Build/Scripts/runTests.sh -h\" to display help and valid options" >&2
@@ -125,7 +125,7 @@ handleDbmsOptions() {
 loadHelp() {
     # Load help text into $HELP
     read -r -d '' HELP <<EOF
-EXT:replacer test runner. Check code styles, lint PHP files and some other details.
+EXT:examples test runner. Check code styles, lint PHP files and some other details.
 
 Usage: $0 [options] [file]
 
@@ -204,7 +204,7 @@ Options:
 
     -p <8.2|8.3|8.4>
         Specifies the PHP minor version to be used
-            - 8.2: use PHP 8.2 (default)
+            - 8.2: use PHP 8.2
             - 8.3: use PHP 8.3
             - 8.4: use PHP 8.4
 
@@ -231,14 +231,14 @@ Options:
         Show this help.
 
 Examples:
-    # Run unit tests using PHP 8.2
-    ./Build/Scripts/runTests.sh -p 8.2 -s unit
+    # Run unit tests using PHP 8.3
+    ./Build/Scripts/runTests.sh -p 8.3 -s unit
 
-    # Run functional tests using PHP 8.3 and MariaDB 10.6 using pdo_mysql
-    ./Build/Scripts/runTests.sh -p 8.3 -s functional -d mariadb -i 10.6 -a pdo_mysql
+    # Run functional tests using PHP 8.4 and MariaDB 10.6 using pdo_mysql
+    ./Build/Scripts/runTests.sh -p 8.4 -s functional -d mariadb -i 10.6 -a pdo_mysql
 
-    # Run functional tests on postgres with xdebug, php 8.3 and execute a restricted set of tests
-    ./Build/Scripts/runTests.sh -x -p 8.3 -s functional -d postgres -- Tests/Functional/DummyTest.php
+    # Run functional tests on postgres with xdebug, php 8.4 and execute a restricted set of tests
+    ./Build/Scripts/runTests.sh -x -p 8.4 -s functional -d postgres -- Tests/Functional/DummyTest.php
 EOF
 }
 
