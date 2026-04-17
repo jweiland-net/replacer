b6e41e8701f81155e7bc6d0f50e2dee1886fb722
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-04-17 13:35:48 +0200

    [TASK] Update .gitignore, runTests.sh defaults, and PHP-CS-Fixer rules

--- Build/cgl/.php-cs-fixer.dist.php
+++ Build/cgl/.php-cs-fixer.dist.php
@@ -37,7 +37,7 @@ return (new Config())
             'header' => $headerComment,
         ],
         // @todo: Switch to @PER-CS2.0 once php-cs-fixer's todo list is done: https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/issues/7247
-        '@PER-CS1x0' => true,
+        '@PER-CS1.0' => true,
         'array_indentation' => true,
         'array_syntax' => ['syntax' => 'short'],
         'cast_spaces' => ['space' => 'none'],
@@ -90,7 +90,7 @@ return (new Config())
         'single_line_comment_style' => ['comment_types' => ['hash']],
         // @todo: Can be dropped once we enable @PER-CS2.0
         'single_line_empty_body' => true,
-        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+        'trailing_comma_in_multiline' => ['elements' => ['arguments', 'arrays', 'match', 'parameters']],
         'whitespace_after_comma_in_array' => ['ensure_single_space' => true],
         'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
 
